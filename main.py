23c22c38f6e4c22fb929f47a2df927324b56a1db
Your Name <you@example.com>
2026-09-18 19:05:18 -0500

    feat: agrega GET /students

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from config import APP_VERSION
+
+app = FastAPI(title="students-api", version=APP_VERSION)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/students")
+def list_students():
+        return[{"id":1, "name": "Ana"}, {"id": 2, "name": "LuiS"}]
